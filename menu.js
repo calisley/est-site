7cadbd17a7753e12397567dcef1c45375404ea97
Calvin Isley <calvin.isley@wustl.edu>
2019-03-27 11:41:34 -0500

    fixing service button

--- menu.js
+++ menu.js
@@ -6,3 +6,21 @@ function closeMenu() {
 	document.getElementsByClassName('sidebar_content')[0].style.display = "none";
 	document.getElementById('menu_button').style.display = 'block';
 }
+
+function checkService(){
+	var inServe = true; 
+	// FOR CHANGING SERVICE STATUS, CHANGE ABOVE TO TRUE OR FALSE. (true for in service, false for out of service.)
+	if (inServe == true) {
+		
+		document.getElementsByClassName('in')[0].style.display = "block";
+		document.getElementsByClassName('in')[1].style.display = "block";
+		document.getElementsByClassName('out')[0].style.display = "none";
+		document.getElementsByClassName('out')[1].style.display = "none";
+	}
+	else{
+		document.getElementsByClassName('in')[0].style.display = "none";
+		document.getElementsByClassName('in')[1].style.display = "none";
+		document.getElementsByClassName('out')[0].style.display = "block";
+		document.getElementsByClassName('out')[1].style.display = "block";	
+	}
+}
